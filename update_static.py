9a22ab6024e25bdea47c2105d34b443bfb62a74f
Marshall <xtremebass2k@gmail.com>
2018-05-18 06:29:38 -0700

    script for getting static content ready for production

---
+++ update_static.py
@@ -0,0 +1,15 @@
+import sys
+from fabric import Connection
+from fabfile import pre_deploy, utils
+
+if __name__ == '__main__':
+    hostname = sys.argv[1]
+    config = utils.load_config('{}.yaml'.format(hostname))
+    c = Connection(config['hostname'])
+
+    print('updating,,,')
+    with c.cd(config['base-dir']):
+        pre_deploy.update_repo(c, config)
+
+        for f in config['static_files']:
+            pre_deploy.update_file(c, config, f)
